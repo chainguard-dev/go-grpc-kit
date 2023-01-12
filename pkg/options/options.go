c45320d730f5a5aba83c6217b109d2803a1e06bc
Kenny Leung <kleung@chainguard.dev>
2023-01-11 17:59:20 -0800

    default to 0
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -31,7 +31,7 @@ var env struct {
 	EnableClientHandlingTimeHistogram      bool `envconfig:"ENABLE_CLIENT_HANDLING_TIME_HISTOGRAM" default:"true"`
 	EnableClientStreamReceiveTimeHistogram bool `envconfig:"ENABLE_CLIENT_STREAM_RECEIVE_TIME_HISTOGRAM" default:"true"`
 	EnableClientStreamSendTimeHistogram    bool `envconfig:"ENABLE_CLIENT_STREAM_SEND_TIME_HISTOGRAM" default:"true"`
-	GrpcClientMaxRetry                     uint `envconfig:"GRPC_CLIENT_MAX_RETRY" default:"5"`
+	GrpcClientMaxRetry                     uint `envconfig:"GRPC_CLIENT_MAX_RETRY" default:"0"`
 }
 
 func init() {
